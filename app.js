00f474c36f805ed518564ece1052827c176a703d
patcharaponworkall-tech <patcharapon.work.all@gmail.com>
2026-09-15 02:07:29 +0700

    Update app.js
    Signed-off-by: patcharaponworkall-tech <patcharapon.work.all@gmail.com>

--- app.js
+++ app.js
@@ -218,7 +218,7 @@ async function createCollage(files) {
     return collageBase64;
 }
 document.addEventListener('DOMContentLoaded', async  function () {
-  await  initializeliff();
+  await  initializeLiff();
 const shiftSelect =
         document.getElementById('shift');
 
